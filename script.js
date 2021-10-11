1987d65a137a81c466ff7f03adf6277a24552810
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-11 14:51:37 +0200

    impostate le condizioni, esercizio completato

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ console.log("l'utente ha " + etaPasseggero + " anni");
 console.log("deve percorrere " + kmPasseggero + " km");
 console.log("quindi il prezzo intero del biglietto è di " + bigliettoIntero + " Euro");
 /* / dati utente */
+
+if (etaPasseggero < 18) {
+    document.getElementById("prezzo_finale").innerHTML = 
+        `
+            sei minorenne, hai uno sconto del 20%, quindi dovrai pagare ${(bigliettoIntero - scontoDaApplicareMinorenni).toPrecision(4)} Euro
+        
+        `
+} else if (etaPasseggero > 65) {
+    document.getElementById("prezzo_finale").innerHTML = 
+    `
+        per lei, è stato applicato uno sconto del 40%, dovrà pagare ${(bigliettoIntero - scontoDaApplicareOver65).toPrecision(4)} Euro
+    
+    `
+} else {
+    document.getElementById("prezzo_finale").innerHTML = 
+    `
+        ... devi pagare ${bigliettoIntero.toPrecision(4)} Euro
+    
+    `
+}
+/* /CONDIZIONI */
